d7866d288ed5a8571e495aeae7fd14ccf05d59c1
Benjamin Wasty <benny.wasty@gmail.com>
2019-02-03 00:38:26 +0100

    finish transaction, fix types

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! Toy Ethereum client closely following the [Yellow Paper](https://github.com/ethereum/yellowpaper/).
 //! Structs, fields and methods are annotated with their formal definition where applicable.
 
+use ethereum_types::{U256, H256};
 use tiny_keccak::keccak256;
 
 /// σ
@@ -22,7 +23,6 @@ impl WorldState {
     }
 }
 
-pub type Hash = [u8; 32];
 pub type Address = [u8; 20];
 
 /// σ[a]
@@ -31,31 +31,31 @@ pub struct AccountState {
     /// A scalar value equal to the number of transactions sent from this address or, 
     /// in the case of accounts with associated code, the number of contract-creations made by 
     /// this account. For account of address a in state σ, this would be formally denoted σ[a]<sub>n</sub>.
-    pub nonce: u64,
+    pub nonce: U256,
     /// A scalar value equal to the number of Wei owned by this address. Formally denoted σ[a]<sub>b</sub>.
-    pub balance: u64, 
+    pub balance: U256, 
     /// A 256-bit hash of the root node of a Merkle Patricia tree that encodes the storage contents 
     /// of the account (a mapping between 256-bit integer values), encoded into the trie as a mapping 
     /// from the Keccak 256-bit hash of the 256-bit integer keys to the RLP-encoded 256-bit integer 
     /// values. The hash is formally denoted σ[a]<sub>s</sub>.
     /// TODO?: It shall be understood that σ[a]<sub>s</sub> is not a ‘physical’ member of the account and does 
     /// not contribute to its later serialisation.
-    pub storage_root: Hash,
+    pub storage_root: H256,
     /// The hash of the EVM code of this account — this is the code that gets executed should this 
     /// address receive a message call; it is immutable and thus, unlike all other fields, cannot 
     /// be changed after construction. All such code fragments are contained in the state database 
     /// under their corresponding hashes for later retrieval. This hash is formally denoted σ[a]<sub>c</sub>, 
     /// and thus the code may be denoted as b, given that KEC(b) = σ[a]<sub>c</sub>.
-    pub code_hash: Hash,
+    pub code_hash: H256,
 }
 
 impl Default for AccountState {
     fn default() -> Self {
         AccountState {
-            nonce: 0,
-            balance: 0,
-            storage_root: keccak256(&[]),
-            code_hash: keccak256(&[]),
+            nonce: 0.into(),
+            balance: 0.into(),
+            storage_root: keccak256(&[]).into(),
+            code_hash: keccak256(&[]).into(),
         }
     }
 }
@@ -65,14 +65,14 @@ impl AccountState {
     /// If the codeHash field is the Keccak-256 hash of the empty string, i.e. σ[a]<sub>c</sub> = KEC(()), 
     /// then the node represents a simple account, sometimes referred to as a “non-contract” account.
     pub fn has_code(&self) -> bool {
-        self.code_hash != keccak256(&[])
+        self.code_hash != keccak256(&[]).into()
     }
 
     /// An account is empty when it has no code, zero nonce and zero balance:
     /// 
     /// EMPTY(σ, a) ≡ σ[a]<sub>c</sub> = KEC(()) ∧ σ[a]<sub>n</sub> = 0 ∧ σ[a]<sub>b</sub> = 0
     pub fn is_empty(&self) -> bool {
-        self.nonce == 0 && self.balance == 0 && !self.has_code()
+        self.nonce.is_zero() && self.balance.is_zero() && !self.has_code()
     }
 }
 
@@ -93,18 +93,50 @@ impl AccountState {
 #[derive(Debug, Default)]
 pub struct Transaction {
     /// A scalar value equal to the number of transactions sent by the sender; formally T<sub>n</sub>.
-    pub nonce: u64,
+    pub nonce: U256,
     /// A scalar value equal to the number of Wei to be paid per unit of _gas_ for all computation costs incurred 
     /// as a result of the execution of this transaction; formally T<sub>p</sub>.
-    pub gas_price: u64,
+    pub gas_price: U256,
     /// A scalar value equal to the maximum amount of gas that should be used in executing this transaction. 
     /// This is paid up-front, before any computation is done and may not be increased later; formally T<sub>g</sub>.
-    pub gas_limit: u64,
+    pub gas_limit: U256,
     /// The 160-bit address of the message call’s recipient or, for a contract creation transaction, ∅, 
     /// used here to denote the only member of B<sub>0</sub>; formally T<sub>t</sub>.
     pub to: Option<Address>,
-    // TODO!!!: continue...
-    // pub value: u64,
+    /// A scalar value equal to the number of Wei to be transferred to the message call’s recipient or, 
+    /// in the case of contract creation, as an endowment to the newly created account; formally T<sub>v</sub>.
+    pub value: U256,
+
+    /// Values corresponding to the signature of the transaction and used to determine the sender of the transaction; 
+    /// formally T<sub>w</sub>, T<sub>r</sub> and T<sub>s</sub>.
+    pub v: u8,
+    pub r: [u8; 32],
+    pub s: [u8; 32],
+
+    /// An unlimited size byte array specifying the EVM-code for the account initialisation procedure, formally 
+    /// T<sub>i</sub>.  
+    /// `init` is an EVM-code fragment; it returns the `body`, a second fragment of code that executes each time the 
+    /// account receives a message call (either through a transaction or due to the internal execution of code). 
+    /// `init` is executed only once at account creation and gets discarded immediately thereafter.  
+    /// Empty for message calls.
+    pub init: Option<Vec<u8>>,
+
+    /// An unlimited size byte array specifying the input data of the message call, formally T<sub>d</sub>.  
+    /// Empty for contract creation transactions.
+    pub data: Option<Vec<u8>>
+}
+
+#[allow(dead_code)]
+impl Transaction {
+    /// S
+    /// 
+    /// Appendix F specifies the function, S, which maps transactions to the sender, and happens through 
+    /// the ECDSA of the SECP-256k1 curve, using the hash of the transaction (excepting the latter three 
+    /// signature fields) as the datum to sign. For the present we simply assert that the sender of a 
+    /// given transaction T can be represented with S(T).
+    pub fn sender(&self) {
+        unimplemented!() // TODO!
+    }
 }
 
 #[cfg(test)]
@@ -117,7 +149,7 @@ mod tests {
         assert!(!acc.has_code());
         assert!(acc.is_empty());
 
-        acc.code_hash = keccak256(&[1, 2, 3]);
+        acc.code_hash = keccak256(&[1, 2, 3]).into();
         assert!(acc.has_code());
         assert!(!acc.is_empty());
     }
