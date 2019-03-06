88b4aa5af82594898f87611ebce039e0867d6f8e
Benjamin Wasty <benny.wasty@gmail.com>
2019-03-06 23:40:48 +0100

    working cors

--- src/lib/rpc/server.rs
+++ src/lib/rpc/server.rs
@@ -1,11 +1,19 @@
 use actix_web::{server, App, HttpRequest};
+use actix_web::middleware::cors::Cors;
 
 fn index(_req: &HttpRequest) -> &'static str {
     "Hello world!"
 }
 
 pub fn start_server() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
+    server::new(|| {
+        App::new().configure(|app| {
+                Cors::for_app(app)
+                    // .allowed_methods(vec!["GET", "POST", "OPTIONS"])
+                    .resource("/", |r| r.f(index))
+                    .register()
+            })
+        })
         .bind("127.0.0.1:8545")
         .unwrap()
         .run();
--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,10 @@ fn main() {
     match opt.commands {
         SubCommand::Node { bootstrap } => {
             if bootstrap {
-                println!(
-                    "not implemented yet, but here's the genesis block: {:?}",
-                    Block::genesis_block()
-                );
+                // println!(
+                //     "not implemented yet, but here's the genesis block: {:?}",
+                //     Block::genesis_block()
+                // );
                 rpc::start_server();
             } else {
                 // TODO!: connect to master node (teth.malkut.net / localhost -> arg...)
