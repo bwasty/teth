f1d743be1da6710c3e671306bcf2e1c5a0260345
Benjamin Wasty <benny.wasty@gmail.com>
2019-03-07 00:31:46 +0100

    replace actix with jsonrpc-ws-server

--- src/lib/rpc/server.rs
+++ src/lib/rpc/server.rs
@@ -1,20 +1,22 @@
-use actix_web::{server, App, HttpRequest};
-use actix_web::middleware::cors::Cors;
-
-fn index(_req: &HttpRequest) -> &'static str {
-    "Hello world!"
-}
+use jsonrpc_ws_server::jsonrpc_core::{IoHandler, Params, types::Error};
+use jsonrpc_ws_server::ServerBuilder;
 
 pub fn start_server() {
-    server::new(|| {
-        App::new().configure(|app| {
-                Cors::for_app(app)
-                    // .allowed_methods(vec!["GET", "POST", "OPTIONS"])
-                    .resource("/", |r| r.f(index))
-                    .register()
-            })
-        })
-        .bind("127.0.0.1:8545")
-        .unwrap()
-        .run();
+    let mut io = IoHandler::new();
+    io.add_method("eth_getBalance", |params| {
+        // let address = 
+        match params {
+            Params::Array(values) => values.get(0)
+                .cloned()
+                .ok_or_else(|| Error::invalid_params("expected 1 param - addresss.")),
+            _ => Err(Error::invalid_params("expected 1 param - addresss."))
+        }
+        // TODO!!!: query worldstate...
+    });
+
+    let server = ServerBuilder::new(io)
+        .start(&"0.0.0.0:8546".parse().unwrap())
+        .expect("Server must start with no issues");
+
+    server.wait().unwrap()
 }
--- teth-web/index.ts
+++ teth-web/index.ts
@@ -1,8 +1,9 @@
 import Web3 from 'web3';
 import { Account } from 'web3-eth-accounts';
-const web3 = new Web3("http://localhost:8545");
+const web3 = new Web3("ws://localhost:8546");
 (window as any).web3 = web3; // for debugging
 
+// TODO!!: use wallet instead...
 let account: Account;
 const storageKey = 'web3-local-account';
 if (localStorage.getItem(storageKey)) {
@@ -17,3 +18,5 @@ if (localStorage.getItem(storageKey)) {
 (window as any).account = account;
 
 document.body.append(`Address: ${account.address}`)
+
+web3.eth.getBalance(account.address)
