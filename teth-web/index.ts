a8085d156fd1d40c8969fd830bdc40ec35174ee2
Benjamin Wasty <benny.wasty@gmail.com>
2019-03-10 09:28:27 +0100

    try web3 transaction signing

--- teth-web/index.ts
+++ teth-web/index.ts
@@ -32,6 +32,17 @@ div.innerHTML = `Address: ${account.address}, `;
     }))
 
     // await provider.send('teth_faucet', [account.address]);
+
+    let tx = {
+        nonce: "0",
+        chainId: "85",
+        to: "0x96F1e2BdcB7645773D3DE58BcCB6223c44fA7D29",
+        value: "42000", 
+        gasPrice: "2",
+        gas: "42000",
+    }
+    // TODO!!: Cannot read property 'sign' of undefined
+    let signedTx = await web3.eth.accounts.signTransaction(tx, account.privateKey);
 })()
 
 // const wallet = web3.eth.accounts.wallet;
